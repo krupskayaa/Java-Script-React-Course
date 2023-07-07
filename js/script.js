edbd7932972d1a5a790ed548395ea0d3789ecbc2
Alona <alyona.krupskaya@gmail.com>
2023-07-07 17:26:37 +0300

    deleted files

--- js/script.js
+++ js/script.js
@@ -767,3 +767,13 @@
 // }
 
 // transferWaitors(restorantData);
+
+// function pow(x, n){
+//     if (n === 1){
+//         return x;
+//     } else {
+//         return x * pow(x, n - 1);
+//     }
+// }
+
+// console.log(pow(2, 3));
